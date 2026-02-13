978e3c8385c3ed72874e130a27f5f4339ab1d980
Jhony279 <lordjhony732@gmail.comm>
2026-02-12 23:41:23 -0800

    Starting to create first function

--- main.cpp
+++ main.cpp
@@ -6,13 +6,21 @@ using namespace std;
 string* reverseArray(string[]);
 void displayArray();
 
+const int ARRAY_SIZE = 5;
+
 /**
  * @brief - Description of main
  * @return 
 */
 int main() {
     string *namesPtr = nullptr;
-    namesPtr = new string[5]{"Janet", "Jeffe", "Jin", "Joe", "Junio"};
+    namesPtr = new string[ARRAY_SIZE]{"Janet", "Jeffe", "Jin", "Joe", "Junio"};
 
     return 0;
+}
+
+string* reverseArray(string nameArray[]){
+    for (int i = 0; i < ARRAY_SIZE; i++){
+        
+    }
 }
