ee58bf6f5e51975c4715f7fba59fb5d5700062ca
Jhony279 <lordjhony732@gmail.comm>
2026-02-13 00:22:56 -0800

    Finished creating both functions and added comments to code

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ using namespace std;
 string* reverseArray(string[]);
 void displayArray(string[]);
 
-const int ARRAY_SIZE = 5;
+const int ARRAY_SIZE = 5, ARRAY_OFFSET = 1;
 
 /**
- * @brief - Description of main
- * @return 
+ * @brief Main function, Creates a dynamic array of strings, reverses it, and
+ *        displays the reversed array
 */
 int main() {
     string *namesPtr = nullptr;
@@ -22,15 +22,24 @@ int main() {
     return 0;
 }
 
+/**
+ * @brief Creates a new array of strings that is the reverse of the passed array
+ * @param nameArray The array of strings to reverse
+ * @return A pointer to the new array of strings
+*/
 string* reverseArray(string nameArray[]){
     string *arrayPtr = new string[ARRAY_SIZE];
 
     for (int i = 0; i < ARRAY_SIZE; i++){
-        *(arrayPtr + i) = *(nameArray + (ARRAY_SIZE - i) - 1);
+        *(arrayPtr + i) = *(nameArray + (ARRAY_SIZE - i) - ARRAY_OFFSET);
     }
     return arrayPtr;
 }
 
+/**
+ * @brief Displays the elements of a string array
+ * @param nameArray The array of strings to display
+*/
 void displayArray(string nameArray[]){
     cout << "Outputting array elements: ";
     // Loop through array and print out each element
