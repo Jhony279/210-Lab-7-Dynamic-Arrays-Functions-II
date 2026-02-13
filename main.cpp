8b3ce677c0e21fb2c3515037b17169e494442e3f
Jhony279 <lordjhony732@gmail.comm>
2026-02-12 22:58:08 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// COMSC-210 | Lab # | Johnathan Perez Baltazar
+
+#include <iostream>
+using namespace std;
+
+// Function Prototypes:
+
+/**
+ * @brief - Description of main
+ * @return 
+*/
+int main() {
+    return 0;
+}
+
+// Function Definitions:
+
+// Reminder lines must be 80 characters or less, no more than one empty line, no magic numbers,
+// and Reminder to comment code where neccessary.
